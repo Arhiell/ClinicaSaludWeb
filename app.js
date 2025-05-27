a8be11ad24ee4bcd0decab88a27bd4d8532a0183
Arhiell <arielayala907.hz@gmail.com>
2025-05-27 08:51:13 -0300

    Modificaciones en la ruta de login, paso a estar en la carpeta Public

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Configurar motor de vistas o servir HTML directamente
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "login.html"));
+  res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 // Rutas
