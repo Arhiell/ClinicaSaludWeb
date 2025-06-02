c0de507f16abf77b9565c1348d503d7e2df02038
Arhiell <arielayala907.hz@gmail.com>
2025-06-02 09:37:13 -0300

    Se coloco sin la clave

--- db/conexion.js
+++ db/conexion.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const conexion = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "bauticapodatabase2000",
+  password: "",
   database: "clinica",
 });
 
